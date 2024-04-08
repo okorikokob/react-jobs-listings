5031e38992b2059c5bfca6a5abd735432b2e7271
Oluwatobi Sholanke <tobisholanke@gmail.com>
2024-04-08 13:10:31 +0300

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
+// eslint-disable-next-line react/prop-types
 const Card = ({children, bg = 'bg-gray-100'}) => {
   return (
     <div className={`${bg} p-6 rounded-lg shadow-md `}>
